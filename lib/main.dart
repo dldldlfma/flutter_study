b8627081a55550b5a69c777346543b9c0a552cd5
SanggunKim <dldldlfma@gmail.com>
2019-05-07 17:14:29 +0900

    add Icon and Image from network

--- lib/main.dart
+++ lib/main.dart
@@ -15,12 +15,32 @@ class MyApp extends StatelessWidget {
           body: new Center(
             child: new Column(
               children: <Widget>[
-                new Text("Hello Flutter "),
+                new Column(
+                children: <Widget>[
+                  new Text("Hello Flutter "),
+                  new Text("Hello Flutter2 "),
+                ]
+            ),
                 new Text("Hello Flutter -"),
-                new AppBar(
-                  title: new Text("Hello MF"),
+
+                new Column(
+                  children: <Widget>[
+                    new AppBar(
+                      title: new Text("Hello"),
+                    ),
+                    new AppBar(
+                      title: new Text("Hello2"),
+                    ),
+                  ]
                 ),
                 new Icon(Icons.android),
+                new Icon(
+                  Icons.favorite,
+                  size: 100.0,
+                  color: Colors.red,
+                ),
+                new Image.network('https://cdn.arstechnica.net/wp-content/uploads/2018/12/7-800x272.jpg')
+
               ],
             ),
           ),
